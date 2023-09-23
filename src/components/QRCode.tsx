128e7296c5a78ab9642ab6a8aeed3eed4be15236
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-23 20:54:42 +0300

    Created QRCode component.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const QRCode = () => {
     return (
         <div>
-            
+              <div>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
         </div>
     );
 };
