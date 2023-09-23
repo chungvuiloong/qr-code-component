19c433fc437ae9609cab819d21af17129df3ab69
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-23 20:56:31 +0300

    Add QRCode component & footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QRCode from './components/QRCode';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <QRCode />
+        <div>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+        </div>
     </div>
   );
 }
