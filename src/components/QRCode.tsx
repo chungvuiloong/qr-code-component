a7ad7849d27e44045939a3b1550ba969ac7e4743
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-23 21:16:06 +0300

    Added container and card.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,12 +1,17 @@
+import { Card, Typography } from '@material-tailwind/react';
 import React from 'react';
 
 const QRCode = () => {
     return (
-        <div>
-              <div>
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-                Coded by <a href="#">Your Name Here</a>.
-            </div>
+        <div className='container'>
+            <Card>
+                <Typography variant='h3' className='font-semibold'>
+                    Improve your front-end skills by building projects
+                </Typography>
+                <Typography variant='paragraph'>
+                    Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level
+                </Typography>
+            </Card>
         </div>
     );
 };
