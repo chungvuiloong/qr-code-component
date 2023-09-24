00a73a3a8d54f95a53f63c35fc6bd4d7af0efb4c
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-24 09:48:48 +0300

    Added borders to Card component.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -5,8 +5,8 @@ import QR_logo from '../assets/images/image-qr-code.png'
 const QRCode = () => {
     return (
         <div className='container mx-auto'>
-            <Card className='w-[300px] shadow-lg p-3 mx-auto text-center'>
-                <img src={QR_logo} alt="QR code" />
+            <Card className='w-[300px] shadow-lg p-3 mx-auto text-center border border-neutral-700'>
+                <img src={QR_logo} alt="QR code" className='rounded-md'/>
                 <Typography variant='h5' className='font-bold'>
                     Improve your front-end skills by building projects
                 </Typography>
