ba163d401f9ed10a9c611685024986734dde81a4
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-24 08:29:26 +0300

    Add QR code image.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,10 +1,12 @@
 import { Card, Typography } from '@material-tailwind/react';
 import React from 'react';
+import QR_logo from '../assets/images/image-qr-code.png'
 
 const QRCode = () => {
     return (
         <div className='container mx-auto'>
-            <Card className='w-fit shadow-lg p-5 mx-auto '>
+            <Card className='w-fit shadow-lg p-3 mx-auto '>
+                <img src={QR_logo} alt="QR code" />
                 <Typography variant='h3' className='font-bold'>
                     Improve your front-end skills by building projects
                 </Typography>
