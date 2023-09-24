932f003632c461525809f6722238a7541d52cdf8
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-24 07:39:08 +0300

    Added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css';
 import QRCode from './components/QRCode';
+import { ThemeProvider } from "@material-tailwind/react";
+
 
 function App() {
-  return (
-    <div >
-        <QRCode />
-        <div>
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+  return (    
+    <ThemeProvider>    
+        <div className='h-[100vh] flex flex-col justify-center mx-auto'>
+            <QRCode />
+            <div className='flex- flex-end'>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
         </div>
-    </div>
+    </ThemeProvider>
+
   );
 }
 
