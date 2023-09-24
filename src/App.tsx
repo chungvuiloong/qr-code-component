286bf5c1b89f0c4b0776da703590322570d6a9d2
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-24 08:13:13 +0300

    Add spaces and underline class.

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,14 @@ function App() {
         <div className='relative h-[100vh] flex flex-col justify-center mx-auto'>
             <QRCode />
                 <div className='py-10 absolute bottom-0 bg-[#262626] w-full text-white text-center'>
-                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-                    Coded by <a href="#">Your Name Here</a>.
+                    Challenge by{" "}
+                    <a href="https://www.frontendmentor.io?ref=challenge" 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    className='underline underline-offset-4'>
+                        Frontend Mentor
+                    </a>. 
+                    Coded with enthusiasm by {""}<a href="#" className='underline underline-offset-4'>Jere Sulovuo (MrJay)</a>.
             </div>
         </div>
     </ThemeProvider>
