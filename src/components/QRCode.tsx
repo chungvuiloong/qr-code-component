dcf673295a9c3625153e7cab474bb65e87a42dd9
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-24 09:56:22 +0300

    IMproved design. Add margin.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -5,12 +5,12 @@ import QR_logo from '../assets/images/image-qr-code.png'
 const QRCode = () => {
     return (
         <div className='container mx-auto'>
-            <Card className='w-[300px] shadow-lg p-3 mx-auto text-center border border-neutral-700'>
-                <img src={QR_logo} alt="QR code" className='rounded-md'/>
-                <Typography variant='h5' className='font-bold'>
+            <Card className='w-[300px] shadow-md p-3 mx-auto text-center border border-neutral-700 rounded-2xl flex gap-5'>
+                <img src={QR_logo} alt="QR code" className='rounded-xl'/>
+                <Typography variant='h5' className='mt-2 font-bold'>
                     Improve your front-end skills by building projects
                 </Typography>
-                <Typography variant='paragraph'>
+                <Typography variant='paragraph' className='mb-5'>
                     Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level
                 </Typography>
             </Card>
